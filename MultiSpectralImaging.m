ccee6c93c4c4b27e1bbedbe5085a8af1454a5cff
Siem Gebrehiwet <102214627+Siem4all@users.noreply.github.com>
2024-12-18 15:19:15 +0200

    two classes

--- MultiSpectralImaging.m
+++ MultiSpectralImaging.m
@@ -15,43 +15,43 @@ classdef MultiSpectralImaging
         function obj = MultiSpectralImaging(wavelengths, total_bands, roi, x, y) 
             obj.wavelengths = wavelengths;
             obj.total_bands = total_bands;
-            obj.roi = roi;
-            obj.si_images = {};
-            obj.num_images = 0; % Initialize count to zero
-            obj.width=x;
-            obj.height=y;
+            obj.roi         = roi;
+            obj.si_images   = {};
+            obj.num_images  = 0; % Initialize count to zero
+            obj.width       =x;
+            obj.height      =y;
         end
         
-        function obj = runMultiSpectral(obj, imageDir, numImages, dir_ind, position)
+        function obj  = runMultiSpectral(obj, imageDir, numImages, dir_ind, position)
             % Initialize empty image for each band based on the roi value
             img_bands =cell(1, 4);
-            for index=1:4
+            for index = 1:4
                 img_bands{index}=zeros(obj.width/2, obj.height/2);
             end
             % Create a structure to hold dynamic variable names
-            data = struct();
+            data      = struct();
             % Ensure the directory is formatted correctly
-            imageDir = strrep(imageDir, '\', '\\');
+            imageDir  = strrep(imageDir, '\', '\\');
             % Get a list of all PNG files in the directory
-            imageFiles = dir(fullfile(imageDir, '*.png'));
+            imageFiles= dir(fullfile(imageDir, '*.png'));
             if isempty(imageFiles)
                 disp('No images found in the specified directory.');
                 return; % Exit the function early
             end
             % Initialize each field of data in a loop
-            fields = {'avg_refl', 'rf_std_error', 'avg_si_refl', 'si_std_error'};
+            fields    = {'avg_refl', 'rf_std_error', 'avg_si_refl', 'si_std_error'};
             for i = 1:length(fields)
                data.(fields{i}) = zeros(1, obj.total_bands);
             end
             % Process images in groups of numImages
             for i = 1:numImages
                 % Construct the full file name
-                filename = fullfile(imageDir, imageFiles(i).name);
+                filename    = fullfile(imageDir, imageFiles(i).name);
                 
                 % Check if the file exists
                 if isfile(filename)
                     % Read the image and Smooth it using imbilatfilt
-                    img =imbilatfilt(imread(filename));
+                    img     =imbilatfilt(imread(filename));
                     
                     % Convert to grayscale if the image is RGB
                     if size(img, 3) == 3
@@ -62,9 +62,9 @@ classdef MultiSpectralImaging
                         new_img=obj.crop_image(img, channel, position);
                         if size(img_bands{channel}) ~= size(new_img)
                             % Resize img_bands{channel} to the size of croppedROI
-                            img_bands{channel} = imresize(img_bands{channel}, [size(new_img, 1), size(new_img, 2)]);
+                            img_bands{channel}       = imresize(img_bands{channel}, [size(new_img, 1), size(new_img, 2)]);
                         end
-                        img_bands{channel}=img_bands{channel}+new_img;
+                        img_bands{channel}           = img_bands{channel}+new_img;
                     end
                 else
                     disp(['Image not found: ', filename]);
@@ -73,20 +73,20 @@ classdef MultiSpectralImaging
             
             for j = 1:obj.total_bands
                 if ~isempty(img_bands{j})  
-                    [x, y] = size(img_bands{j});
+                    [x, y]              = size(img_bands{j});
                     % Increment num_images for each succesi  ppsfully processed image
-                    obj.num_images = obj.num_images + 1; 
+                    obj.num_images      = obj.num_images + 1; 
                     % Normalize the image
-                    img=obj.normalize_image(img_bands{j}./ numImages, j, position);
+                    img                 = obj.normalize_image(img_bands{j}./ numImages, j, position);
                     %img=im2uint8(img_bands{j}./ numImages);
-                    data.rf_std_error(j) = std2(img) / sqrt((x + y)); % Standard error calculation
-                    data.avg_refl(j) = mean2(img);  
+                    data.rf_std_error(j)= std2(img) / sqrt((x + y)); % Standard error calculation
+                    data.avg_refl(j)    = mean2(img);  
                     % Calculate K/S or average si
-                    si =obj.cal_absop_to_scatt_ratio(img_bands{j});
-                    si_uint8 = obj.normalize_image(si./ numImages,j, position);
+                    si                  =obj.cal_absop_to_scatt_ratio(img_bands{j});
+                    si_uint8            = obj.normalize_image(si./ numImages,j, position);
                     %si_uint8=im2uint8(si./ numImages);
                     data.avg_si_refl(j) = mean2(si_uint8);
-                    data.si_std_error(j) = std2(si_uint8) / sqrt((x + y));
+                    data.si_std_error(j)= std2(si_uint8) / sqrt((x + y));
                     % Store the processed image
                     obj.si_images{obj.num_images} = si_uint8;
                     % Display the fourth band images before and after
@@ -119,24 +119,24 @@ classdef MultiSpectralImaging
             % Define the row and column indices for each band
            row_indices = [rows(1,1), rows(1,2); rows(1,1), rows(1,2); rows(2,1), rows(2,2); rows(2,1), rows(2,2)];
            col_indices = [cols(1,1), cols(1,2); cols(2,1), cols(2,2); cols(1,1), cols(1,2); cols(2,1), cols(2,2)];
-           new_img=img_db(row_indices(j, 1):row_indices(j, 2), col_indices(j, 1):col_indices(j, 2));
+           new_img        =img_db(row_indices(j, 1):row_indices(j, 2), col_indices(j, 1):col_indices(j, 2));
            if obj.roi==true
                % Extract the First ROI Positions
-                  x = position(:, 1);
-                  y = position(:, 2);
+                  x        = position(:, 1);
+                  y        = position(:, 2);
 
                   % Create a binary mask using roipoly
-                  mask = roipoly(new_img, x, y);
+                  mask      = roipoly(new_img, x, y);
 
                  %Find the bounding box of the ROI
                  [row, col] = find(mask); % Get the coordinates of the ROI in the mask
                  % Determine the bounding box
-                 xMin = min(col);
-                 xMax = max(col);
-                 yMin = min(row);
-                 yMax = max(row);
+                 xMin       = min(col);
+                 xMax       = max(col);
+                 yMin       = min(row);
+                 yMax       = max(row);
                  %Crop the image using the bounding box
-                 new_img = new_img(yMin:yMax, xMin:xMax, :);
+                 new_img    = new_img(yMin:yMax, xMin:xMax, :);
            end
         end
         
@@ -155,14 +155,14 @@ classdef MultiSpectralImaging
 
                 % Read and convert the images to grayscale
                 white = rgb2gray(imread('C:\\Users\\DELL\\Documents\\MATLAB\\res\\images\\w.png'));
-                dark = rgb2gray(imread('C:\\Users\\DELL\\Documents\\MATLAB\\res\\images\\b.png'));
+                dark  = rgb2gray(imread('C:\\Users\\DELL\\Documents\\MATLAB\\res\\images\\b.png'));
 
                 % Check for division by zero
                 if all(white(:) == dark(:))
                      error('White and dark reference images are identical. Normalization cannot be performed.');
                 end
-                cropped_dark=obj.crop_image(dark, j, position);
-                cropped_white=obj.crop_image(white, j, position);
+                cropped_dark  = obj.crop_image(dark, j, position);
+                cropped_white = obj.crop_image(white, j, position);
                 % Normalize the image
                 normalize_img = (img - cropped_dark) ./(cropped_white - cropped_dark);
                 normalize_img = max(0, min(1, normalize_img));  % Clipping to [0, 1]
@@ -185,9 +185,9 @@ classdef MultiSpectralImaging
 
         function exportData(~, avg_refl, rf_std_error, avg_si_refl, si_std_error, dir_ind)
             % Create dynamic filenames using the provided directory index
-            avg_si = sprintf('Avg si %d', dir_ind);  % Filename for average si data
-            avg_Ref = sprintf('Avg reflection %d', dir_ind); % Filename for average reflection data
-            si_filename = strcat(avg_si, '.mat');      % Complete filename for si data
+            avg_si       = sprintf('Avg si %d', dir_ind);  % Filename for average si data
+            avg_Ref      = sprintf('Avg reflection %d', dir_ind); % Filename for average reflection data
+            si_filename  = strcat(avg_si, '.mat');      % Complete filename for si data
             avg_filename = strcat(avg_Ref, '.mat');          % Complete filename for reflection data
 
             % Check if the average reflection file exists and remove it if it does
@@ -207,13 +207,13 @@ classdef MultiSpectralImaging
         end
         function graph_coloring(obj,i)
             % color maping
-            name = "Ester";
+            name       = "Ester";
             if i>8
-                name="Siem";
+                name   = "Siem";
             end
             folder_name="Before";
             image_index=i;
-            band_num = @(i) mod(i, 4) + (mod(i, 4) == 0) * 4; % Define the lambda expression
+            band_num   = @(i) mod(i, 4) + (mod(i, 4) == 0) * 4; % Define the lambda expression
             figure;
             for j=1:2:4
                 % Create a new figure
@@ -222,7 +222,7 @@ classdef MultiSpectralImaging
                 end
                 % Calculate mean and standard error
                 mean_val = mean2(obj.si_images{image_index}(:));
-                std_dev = std2(obj.si_images{image_index});
+                std_dev  = std2(obj.si_images{image_index});
                 subplot(2,2,j)
                 imagesc(obj.si_images{image_index}); % Display the data as an image
                 axis square; % Correct the axis orientation
@@ -237,7 +237,7 @@ classdef MultiSpectralImaging
                 ylabel('Y'); % Label the Y-axis
                 hold on;
                 subplot(2,2,j+1)
-                h=histogram(obj.si_images{image_index}, 30); % Adjust number of bins as needed
+                h         = histogram(obj.si_images{image_index}, 30); % Adjust number of bins as needed
                 % Find the maximum value of the histogram for positioning text
                 max_value = max(h.Values);
                 % Add mean and std as text labels
